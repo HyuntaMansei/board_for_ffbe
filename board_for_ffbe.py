7c1a080124fea81d8d4850e50df1cc777e252894
HyuntaMansei <jchoi82kor@gmail.com>
2023-09-21 22:15:27 +0900

    some

--- board_for_ffbe.py
+++ board_for_ffbe.py
@@ -2,26 +2,44 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import board_manger
+from datetime import date
+import copy
 
 def write_log(attacker=None, defender=None, stars=None, log=None):
     st.session_state.attack_count += 1
-    global df_log
     msg = f"공격 #{st.session_state.attack_count} - 공격자:{attacker}, 방어자:{defender}, 획득별:{stars}"
     log.append(msg)
     new_log = {
-        "count":st.session_state.attack_count, "attacker":attacker, "defender":defender, "stars":stars
+        "attack_count":st.session_state.attack_count, "attacker":attacker, "defender":defender, "stars":stars
     }
-    st.session_state.df_log = df_log.append(new_log, ignore_index=True)
-    df_log = st.session_state.df_log
-    print(df_log)
-
+    st.session_state.df_defence_log = st.session_state.df_defence_log.append(new_log, ignore_index=True)
+def write_attackers(attackers_df:pd.DataFrame, guild_name=None):
+    columns_to_write = [
+        "inuse","guild_name","member_name", 'match_date'
+    ]
+    attackers_to_write = copy.deepcopy(attackers_df)
+    attackers_to_write['inuse'] = 'y'
+    attackers_to_write['guild_name'] = guild_name
+    if ref_date:
+        attackers_to_write['match_date'] = ref_date
+        attackers_to_write = attackers_to_write[columns_to_write]
+        bm.write_attackers(attackers_to_write, ref_date)
+    else:
+        attackers_to_write['match_date'] = date.today()
+        attackers_to_write = attackers_to_write[columns_to_write]
+        bm.write_attackers(attackers_to_write, date.today())
+    print(attackers_df)
+    print(attackers_to_write)
+def fetch_attackers(guild_name, ref_date=None):
+    res_df = bm.fetch_attackers(guild_name, ref_date)
+    return res_df
 def process_logs():
-    global df_defenders
+    global defender_board
+    df_defence_log = st.session_state.df_defence_log
     for d in defenders['defender_name']:
-    # d = "티파록하트"
-        defence_count = df_log[df_log['defender']==d]['stars'].count()
-        remaining_stars = 3 - df_log[df_log['defender']==d]['stars'].sum()
-        lost_stars_list = df_log[df_log['defender']==d]['stars'].tolist()
+        defence_count = df_defence_log[df_defence_log['defender'] == d]['stars'].count()
+        remaining_stars = 3 - df_defence_log[df_defence_log['defender'] == d]['stars'].sum()
+        lost_stars_list = df_defence_log[df_defence_log['defender'] == d]['stars'].tolist()
         lost_starts_1st = lost_stars_list[0] if defence_count > 0 else None
         lost_starts_2nd = lost_stars_list[1] if defence_count > 1 else None
         lost_starts_3rd = lost_stars_list[2] if defence_count > 2 else None
@@ -31,53 +49,86 @@ def process_logs():
             '방횟':defence_count, '잔별':remaining_stars,
             '1방':lost_starts_1st, '2방':lost_starts_2nd, '3방':lost_starts_3rd, '4방':lost_starts_4th, '5방':lost_starts_5th
         }
-        df_defenders.loc[d] = new_row
+        defender_board.loc[d] = new_row
+    # Alive Attackers
+    # New deck
+    # 1Attack ali
 
-if 'log' not in st.session_state:
-    st.session_state.log = []
-log = st.session_state.log
+# session variables
+if 'attack_log' not in st.session_state:
+    st.session_state.attack_log = []
+attack_log = st.session_state.attack_log
 if 'attack_count' not in st.session_state:
     st.session_state.attack_count = 0
-if 'df_log' not in st.session_state:
-    columns = ["count", "attacker", "defender", "stars"]
-    st.session_state.df_log = pd.DataFrame(columns=columns)
-df_log = st.session_state.df_log
-
-#global variables
+if 'df_defence_log' not in st.session_state:
+    columns = ["attack_count", "attacker", "defender", "stars"]
+    st.session_state.df_defence_log = pd.DataFrame(columns=columns)
+columns_to_make = ['guild_name', 'member_name']
+if 'attackers' not in st.session_state:
+    st.session_state.attackers = pd.DataFrame(columns=columns_to_make)
+    st.session_state.attackers.reset_index()
+# global variables
 bm = board_manger.BoardManager()
-attackers = bm.fetch_attackers()
+alive_attackers = []
+
 defenders = bm.fetch_defenders()
-print("AA")
-print(defenders)
 columns = ['si','잔별','방횟','1방','2방','3방','4방','5방']
-df_defenders = pd.DataFrame(columns=columns, index=defenders['defender_name'].tolist())
-print(df_defenders)
-# df_defenders = pd.DataFrame(columns=columns, index=defenders['defender_name'].tolist())
+defender_board = pd.DataFrame(columns=columns, index=defenders['defender_name'].tolist())
+alive_defenders = []
+
+# Data input
+ref_date = st.date_input("기준날짜")
+ref_date
+st.header("공격자")
+opp_guild_name = st.text_input("길드명")
+
+if st.button("공격자 불러오기"):
+    st.session_state.attackers = fetch_attackers(guild_name=opp_guild_name,ref_date=ref_date)[columns_to_make]
+    st.session_state.attackers.reset_index()
+    # attackers = fetch_attackers(guild_name=opp_guild_name)[columns_to_make]
+    # st.session_state.attackers.reset_index()
+if len(st.session_state.attackers) == 0:
+    st.session_state.attackers.loc[0] = {"guild_name":{opp_guild_name}, "member_name":""}
+st.session_state.attackers = st.data_editor(st.session_state.attackers, num_rows="dynamic", width=1000, hide_index=True)
+if st.button("공격자 저장하기"):
+    print("Before: ", st.session_state.attackers)
+    st.dataframe(st.session_state.attackers, width=1000)
+    write_attackers(attackers_df=st.session_state.attackers, guild_name=opp_guild_name)
 
 # Process logs
 process_logs()
 
-alive_attackers = attackers
-alive_attackers.sort()
-alive_defenders = defenders['defender_name'].tolist()
-alive_defenders.sort()
+# alive_attackers = attackers['member_name'].tolist()
+# alive_attackers.sort()
+# alive_defenders = defenders['defender_name'].tolist()
+# alive_defenders.sort()
 
-attacker = st.selectbox("Attacker", [""] + alive_attackers)
-defender = st.selectbox("Defender", [""] + alive_defenders)
+
+attacker = st.selectbox(f"Attacker({len(alive_attackers)})", [""] + alive_attackers)
+defender = st.selectbox(f"Defender({len(alive_defenders)})", [""] + alive_defenders)
 stars = st.radio("Acq. Stars ", [0, 1, 2, 3])
 
 kwargs = {
     "attacker":attacker,
     "defender":defender,
     "stars":stars,
-    "log":log
+    "log":attack_log
 }
-
 if st.button("입력", on_click=write_log, kwargs=kwargs):
     st.write("Writing Log")
-
-st.session_state.df_log = st.experimental_data_editor(df_log, num_rows="dynamic")
-df_log = st.session_state.df_log
+st.session_state.df_defence_log = st.data_editor(st.session_state.df_defence_log, num_rows="dynamic", hide_index=True, width=1000)
+if st.button("로그저장"):
+    df_to_write = copy.deepcopy(st.session_state.df_defence_log)
+    df_to_write['match_date'] = date.today()
+    print(f"Columns(saving log): {st.session_state.df_defence_log.columns}")
+    bm.write_log_to_server(df_to_write)
+if st.button("로그 불러오기"):
+    print(f"Columns(Loading log): {st.session_state.df_defence_log.columns}")
+    st.session_state.df_defence_log = bm.fetch_guild_battle_log(ref_date)[st.session_state.df_defence_log.columns]
+if st.button("로그 초기화"):
+    if not st.session_state.df_defence_log.empty:
+        st.session_state.df_defence_log = st.session_state.df_defence_log.iloc[0:0]
+    bm.delete_log_in_server(ref_date)
 col_to_show =['잔별', '방횟', '1방', '2방', '3방']
-df_defenders[col_to_show]
-log
+st.table(defender_board[col_to_show].style.format("{:.0f}"))
+attack_log
--- board_manger.py
+++ board_manger.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import pandas
 import pandas as pd
+from datetime import date
 
 def connect_db():
     db_connection = mysql.connector.connect(
@@ -27,6 +28,26 @@ def fetch_data_with_col_names(sql):
     cursor.close()
     conn.close()
     return res, col_names
+def fetch_data_as_df(sql):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    col_names = cursor.column_names
+    cursor.close()
+    conn.close()
+    df = pd.DataFrame(res, columns=col_names)
+    return df
+def fetch_data_with_col_names_as_df_with_value(sql, value):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(sql, value)
+    res = cursor.fetchall()
+    col_names = cursor.column_names
+    cursor.close()
+    conn.close()
+    df = pd.DataFrame(res, columns=col_names)
+    return df
 def write_to_server(sql, values=None):
     conn = connect_db()
     cursor = conn.cursor()
@@ -43,21 +64,81 @@ def write_to_server(sql, values=None):
     conn.commit()
     conn.close()
     return res
-
+def write_df_to_server(table_name, df_to_write:pd.DataFrame):
+    sql_for_col_names = f"""select * from {table_name} LIMIT 1"""
+    _, col_names = fetch_data_with_col_names(sql_for_col_names)
+    common_col_names = [c for c in col_names if c in df_to_write.columns]
+    print(common_col_names)
+    str_for_cols = ','.join(common_col_names)
+    str_for_vals = ','.join(['%s']*len(common_col_names))
+    sql = f"""INSERT INTO {table_name} ({str_for_cols})
+    VALUES ({str_for_vals})"""
+    print(sql)
+    for i, d in df_to_write.iterrows():
+        write_to_server(sql, tuple(d.tolist()))
 class BoardManager:
     def __init__(self):
         pass
-    def fetch_attackers(self):
-        return [
-            "riamo",
-            "aellan",
-            "kingd",
-            "adem",
-            "nidhogg"
-        ]
+
+    def delete_log_in_server(self, ref_date):
+        table_to_write = 'guild_battle_log_tb'
+        sql_for_delete = f"""DELETE FROM {table_to_write} WHERE match_date = %s"""
+        print(sql_for_delete)
+        write_to_server(sql_for_delete, (ref_date,))
+    def write_log_to_server(self, df_to_write:pd.DataFrame):
+        table_to_write = 'guild_battle_log_tb'
+        if not df_to_write.empty:
+            sql_for_delete = f"""DELETE FROM {table_to_write} WHERE match_date = %s"""
+            write_to_server(sql_for_delete, (df_to_write['match_date'][0],))
+        write_df_to_server(table_to_write, df_to_write)
+    def fetch_guild_battle_log(self, ref_date):
+        table_to_fetch = 'guild_battle_log_tb'
+        sql = f"""SELECT * FROM {table_to_fetch} WHERE match_date = '{ref_date}'"""
+        return fetch_data_as_df(sql)
+    def fetch_attackers(self, guild_name, ref_date=None):
+        latest_date_sql = "SELECT MAX(match_date) AS latest_date FROM opponent_guild_members_tb WHERE guild_name=%s"
+        query_value = (guild_name,)
+        res = write_to_server(latest_date_sql,query_value)
+        print(res)
+        latest_date = None
+        for r in res:
+            latest_date = r[0]
+            print("Guild name exist")
+        print("Latest_date:", latest_date)
+        if ref_date:
+            sql = f"""select * from opponent_guild_members_tb where guild_name = %s AND match_date=%s"""
+            value = (guild_name, ref_date)
+            df = fetch_data_with_col_names_as_df_with_value(sql, value)
+        else:
+            sql = f"""select * from opponent_guild_members_tb where guild_name = %s"""
+            value = (guild_name,)
+        df = fetch_data_with_col_names_as_df_with_value(sql, value)
+        df.reset_index()
+        return df
     def fetch_defenders(self):
-        conn = connect_db()
         sql = """select * from guild_members_tb where inuse='y';"""
         data, columns = fetch_data_with_col_names(sql)
         df = pandas.DataFrame(data, columns=columns)
         return df
+    def write_attackers(self, attackers:pd.DataFrame=None, t_date=None):
+        if t_date == date.today():
+            print("Today is the day, writing opponent's guild members.")
+            sql = """DELETE FROM opponent_guild_members_tb WHERE match_date = %s"""
+            value = (t_date,)
+            print(sql)
+            print(value)
+            write_to_server(sql, value)
+            col_names_to_write = attackers.columns
+            cols = ','.join(col_names_to_write)
+            val_str = ','.join(['%s']*len(col_names_to_write))
+            print(cols)
+            print(val_str)
+            for i, a in attackers.iterrows():
+                sql = f"""INSERT INTO opponent_guild_members_tb ({cols}) 
+                VALUES ({val_str})
+                """
+                print(sql)
+                values = tuple(a.tolist())
+                write_to_server(sql, values)
+        else:
+            pass
